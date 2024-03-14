8bc2eaea54d4916bbceebaa9522aeeabd4f96ac7
ripon <riponmondal149@gmail.com>
2024-03-15 03:32:02 +0600

    recap set router and navbar for careerhub

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,23 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Root from './components/Root/Root';
 import Home from './components/Home/Home';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
         element: <Home></Home>,
-      }
+      },
+      {
+        path: '/applied',
+        element: <AppliedJobs></AppliedJobs>,
+      },
     ]
   },
 ]);
