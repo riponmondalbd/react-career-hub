85646714c84080fcdd93ed81e78562517abc3abc
ripon <riponmondal149@gmail.com>
2024-03-17 01:04:55 +0600

    add icons ans handle show all jobs

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,6 +5,9 @@ import Job from "../Job/Job";
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
 
+    // this is not the best way to load show all data
+    const [dataLength, setDatalength] = useState(4);
+
     useEffect(() => {
         fetch('jobs.json')
             .then(res => res.json())
@@ -19,8 +22,11 @@ const FeaturedJobs = () => {
             </div>
             <div className="grid grid-cols-2 gap-6">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className={dataLength === jobs.length ? 'hidden' : 'flex justify-center'}>
+                <button onClick={() => setDatalength(jobs.length)} className="btn btn-primary">Show All Jobs</button>
             </div>
         </div>
     );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,16 +1,23 @@
+import { SlLocationPin } from "react-icons/sl";
+import { AiOutlineDollar } from "react-icons/ai";
+
 
 
 const Job = ({ job }) => {
     const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
-            <figure><img src={logo} alt="Shoes" /></figure>
+            <figure><img className="h-12" src={logo} alt="logo" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{job_title}</h2>
                 <p>{company_name}</p>
                 <div>
                     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">{remote_or_onsite}</button>
                     <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#7E90FE]">{job_type}</button>
+                </div>
+                <div className="mt-4 flex">
+                    <h2 className="flex mr-4"><SlLocationPin className="text-2xl mr-2" />{location}</h2>
+                    <h2 className="flex"><AiOutlineDollar className="text-2xl mr-2" />{salary}</h2>
                 </div>
                 <div className="card-actions">
                     <button className="btn btn-primary">View Details</button>
