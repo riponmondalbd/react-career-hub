fec02a500bea8479ea7f402fb90b1687403dfd60
riponmondalbd <riponmondal149@gmail.com>
2024-08-02 02:55:45 +0600

    clean previous code and setup the project from stratch

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Root from './components/Root/Root';
-import Home from './components/Home/Home';
-import AppliedJobs from './components/AppliedJobs/AppliedJobs';
-import ErrorPage from './components/ErrorPage/ErrorPage';
-import JobDetails from './components/JobDetails/JobDetails';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root></Root>,
-    errorElement: <ErrorPage></ErrorPage>,
-    children: [
-      {
-        path: '/',
-        element: <Home></Home>,
-      },
-      {
-        path: '/applied',
-        element: <AppliedJobs></AppliedJobs>,
-      },
-      {
-        path: '/job/:id',
-        element: <JobDetails></JobDetails>,
-        loader: () => fetch('../jobs.json'), //don't load all data load only what you need
-      },
-    ]
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
