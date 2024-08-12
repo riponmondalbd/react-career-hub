d450b83445dbd38503bc12f4d072a5abeb2f8153
riponmondalbd <riponmondal149@gmail.com>
2024-08-13 02:38:54 +0600

    implement job filter

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -5,6 +5,23 @@ import { getStoredJobApplication } from "../../utility/localStorage";
 const AppliedJobs = () => {
   const jobs = useLoaderData();
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
+
+  const handleJobsFilter = (filter) => {
+    if (filter === "all") {
+      setDisplayJobs(appliedJobs);
+    } else if (filter === "remote") {
+      const remoteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Remote"
+      );
+      setDisplayJobs(remoteJobs);
+    } else if (filter === "onsite") {
+      const onSiteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Onsite"
+      );
+      setDisplayJobs(onSiteJobs);
+    }
+  };
 
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
@@ -21,8 +38,9 @@ const AppliedJobs = () => {
 
       // console.log(jobs, storedJobIds, jobsApplied);
       setAppliedJobs(jobsApplied);
+      setDisplayJobs(jobsApplied);
     }
-  }, []);
+  }, [jobs]);
   return (
     <div>
       <h2 className="text-2xl">Jobs I am applied: {appliedJobs.length}</h2>
@@ -30,20 +48,20 @@ const AppliedJobs = () => {
       <details className="dropdown">
         <summary className="btn m-1">open or close</summary>
         <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-          <li>
+          <li onClick={() => handleJobsFilter("all")}>
             <a>All</a>
           </li>
-          <li>
+          <li onClick={() => handleJobsFilter("remote")}>
             <a>Remote</a>
           </li>
-          <li>
+          <li onClick={() => handleJobsFilter("onsite")}>
             <a>Onsite</a>
           </li>
         </ul>
       </details>
 
       <ul>
-        {appliedJobs.map((job) => (
+        {displayJobs.map((job) => (
           <li key={job.id}>
             <span>
               {job.job_title} {job.company_name} {job.remote_or_onsite}
