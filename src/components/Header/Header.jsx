88d34d83adeb36c317fc6ce6479271767da48fcc
riponmondalbd <riponmondal149@gmail.com>
2024-08-12 18:39:27 +0600

    set page layout and display featured jobs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -48,13 +48,15 @@ const Header = () => {
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">CareerHub</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn text-white bg-gradient-to-r from-[#7E90FE] to-[#9873FF]">
+          Start Applying
+        </a>
       </div>
     </div>
   );
